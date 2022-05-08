13014ffe3d299b1e0c74ccecd45dba02d307ecb3
La-Velle <lavellesine@gmail.com>
2022-05-08 21:35:13 +0100

    bookings controller & seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+pets = Pet.create(
+  [
+  { name: 'Money too tight too mention', jpg_url: ""},
+  { name: 'Daisy', jpg_url: ""},
+  { name: 'Dargo', jpg_url: ""},
+  { name: 'Trexy', jpg_url: ""},
+  ]
+  )
+Character.create(
+  name: 'racehorse', pet: pets.first),
+  (name: 'cow', pet: pets.second),
+  (name: 'dragon', pet: pets.third),
+  (name: 'trex', pet: pets.fourth),
+
+  puts "done"
