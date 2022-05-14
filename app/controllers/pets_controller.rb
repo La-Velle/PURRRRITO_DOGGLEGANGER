75ee5cd528fda08bfae328fe39ec39b07d93d2dc
La-Velle <lavellesine@gmail.com>
2022-05-14 15:22:50 +0100

    small fixes to front end and 2nd geocode map

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -2,14 +2,15 @@ class PetsController < ApplicationController
 
   def index
     #@pet = Pet.all
-    @pet = policy_scope(Pet).order(created_at: :desc)
-    @markers =@pets.geocoded.map do |pet|
+    @pets = policy_scope(Pet).order(created_at: :desc)
+    @markers = @pets.geocoded.map do |pet|
       {
         lat: pet.latitude,
-        lng: pet.longitude
-        info_window: render_to_string(partial: "info_window", locals: { pet: pet })
+        lng: pet.longitude,
+        info_window: render_to_string(partial: "info_window", locals: { pet: pet }),
         image_url: helpers.asset_url("https://cdn5.vectorstock.com/i/1000x1000/47/94/dinosaur-excavation-icon-vector-16674794.jpg")
       }
+    end
   end
 
   def new
@@ -42,10 +43,10 @@ class PetsController < ApplicationController
     @pet.destroy
     redirect_to pets_path
   end
-end
 
-private
+  private
 
-def pet_params
-  params.require(:pet).permit(:name, :description, :price, :availability)
+  def pet_params
+    params.require(:pet).permit(:name, :description, :price, :availability)
+  end
 end
