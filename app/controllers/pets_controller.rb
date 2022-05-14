340cf6f8ece2ccda6c4bebdfe04d986c5efd9216
Anna Godfrey <atagodfrey@gmail.com>
2022-05-14 13:46:59 +0100

    resolving conflicts

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -2,18 +2,19 @@ class PetsController < ApplicationController
 
 
   def index
-    #@pet = Pet.all
+    # @pet = Pet.all
 
     @pets = Pet.all
     @pets = policy_scope(Pet).order(created_at: :desc)
 
-    @markers =@pets.geocoded.map do |pet|
-      {
-        lat: pet.latitude,
-        lng: pet.longditude,
-        info_window: render_to_string(partial: "info_window", locals: { pet: pet }),
-        image_url: helpers.asset_url("REPLACE_THIS_WITH_YOUR_IMAGE_IN_ASSETS")
-      }
+#     @markers = @pets.geocoded.map do |pet|
+#       {
+#         lat: pet.latitude,
+#         lng: pet.longditude,
+#         info_window: render_to_string(partial: "info_window", locals: { pet: pet }),
+#         image_url: helpers.asset_url("REPLACE_THIS_WITH_YOUR_IMAGE_IN_ASSETS")
+#       }
+#     end
   end
 
   def new
@@ -43,10 +44,10 @@ class PetsController < ApplicationController
     @pet.destroy
     redirect_to pets_path
   end
-end
 
-private
+  private
 
-def pet_params
-  params.require(:pet).permit(:name, :description, :price, :availability)
+  def pet_params
+    params.require(:pet).permit(:name, :description, :price, :availability)
+  end
 end
