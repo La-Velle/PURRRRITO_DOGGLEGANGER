eaf38eadfa148bf8f71934a920a83b68e3e9a44a
Anna Godfrey <atagodfrey@gmail.com>
2022-05-21 11:33:08 +0100

    color changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2022_05_14_133940) do
-
+ActiveRecord::Schema.define(version: 2022_05_20_145449) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
