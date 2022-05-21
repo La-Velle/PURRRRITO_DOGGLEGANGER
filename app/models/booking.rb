f0ab67d005b6677da409ee47ad1fc23e7d8859f8
La-Velle <lavellesine@gmail.com>
2022-05-21 12:27:13 +0100

    lost code pics and text

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,3 +3,12 @@ class Booking < ApplicationRecord
   # belongs_to :user_id
   validates :user_id, uniqueness
 end
+
+
+# needs to be added ? to use simple calender gem
+name:string
+start_time:datetime presence:true
+end_time:datetime presence:true
+user:references
+# By default the gem looks for a start_time attribute to display the events,
+# so make sure your model uses this name too.
