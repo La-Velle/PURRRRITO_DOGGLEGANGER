9cda031d3ab443b2edc079cb7aa2196f03af96eb
Anna Godfrey <atagodfrey@gmail.com>
2022-05-21 10:24:42 +0100

    background color

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,25 +1,29 @@
 class PetsController < ApplicationController
   skip_before_action :authenticate_user!
+
   def index
     @pets = policy_scope(Pet).order(created_at: :desc)
-    @markers = @pets.geocoded.map do |pet|
-      {
-        lat: pet.latitude,
-        lng: pet.longitude,
-        info_window: render_to_string(partial: "info_window", locals: { pet: pet }),
-        image_url: helpers.asset_url("https://cdn5.vectorstock.com/i/1000x1000/47/94/dinosaur-excavation-icon-vector-16674794.jpg")
-      }
-    end
+    # @markers = @pets.geocoded.map do |pet|
+    #   {
+    #     lat: pet.latitude,
+    #     lng: pet.longitude,
+    #     info_window: render_to_string(partial: "info_window", locals: { pet: pet }),
+    #     image_url: helpers.asset_url("https://cdn5.vectorstock.com/i/1000x1000/47/94/dinosaur-excavation-icon-vector-16674794.jpg")
+    #   }
+    # end
     if params[:query].present?
-      @pets = Pet.search_by_name_and_location(params[:query])
+      @pets = (params[:query])
+      if @pets.empty?
+        redirect_back(fallback_location: root_path)
+        flash[:alert] = "pet not found :("
+      end
     else
-      @pets = policy_scope(Pet)
+      @pets = Pet.all
     end
   end
 
   def show
     @booking = Booking.new
-    @bookings = pet.bookings
   end
 
   def new
